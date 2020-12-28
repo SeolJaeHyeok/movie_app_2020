d54cb86f7c1de90dfcddec01470f826970410416
seoljaehyeok <milkboy2564@naver.com>
2020-12-28 14:41:30 +0900

    State와 component life cycle

--- src/App.js
+++ src/App.js
@@ -1,72 +1,46 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "sandwich",
-    image:
-      "https://iknowallnews.com/wp-content/uploads/2018/05/nintchdbpict000143036616.jpg",
-    rating: 5.0,
-  },
-  {
-    id: 2,
-    name: "kimchi",
-    image:
-      "https://mygutmatters.com/wp-content/uploads/2017/12/sous-kimchi-1.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: "bulgogi",
-    image:
-      "https://barefeetinthekitchen.com/wp-content/uploads/2014/11/bulgogi-4.jpg",
-    rating: 4.8,
-  },
-  {
-    id: 4,
-    name: "ramen",
-    image: "https://i.ytimg.com/vi/B8y3SSmz4sg/maxresdefault.jpg",
-    rating: 4.7,
-  },
-  {
-    id: 5,
-    name: "bossam",
-    image:
-      "https://www.seriouseats.com/2018/12/20181204-bossam-vicky-wasik-9-1500x1125.jpg",
-    rating: 4.6,
-  },
-];
+//State를 사용하려면 Class component를 써야만 한다.
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-function Food({ name, image, rating }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h4>{rating}/5.0</h4>
-      <img src={image} alt={name} width="400" height="400" />
-    </div>
-  );
-}
+  add = () => {
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
+  };
+  sub = () => {
+    this.setState((current) => ({
+      count: current.count - 1,
+    }));
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
+  // component life cycle
+  //component가 mount(만들어졌을 때)됐을 때
+  componentDidMount() {
+    console.log("Component rendered");
+  }
+  //component가 update(업데이트)됐을 때
+  componentDidUpdate() {
+    console.log("Component updated");
+  }
+  //component가 unmount(죽었을 때)됐을 때 ex)다른 페이지로 이동할 때
+  componentWillUnmount() {
+    console.log("Component unmount");
+  }
 
-function App() {
-  return (
-    <div className="App">
-      {foodILike.map((current) => (
-        <Food
-          key={current.id}
-          name={current.name}
-          image={current.image}
-          rating={current.rating}
-        />
-      ))}
-    </div>
-  );
+  render() {
+    console.log("I'm redering");
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.sub}>Sub</button>
+      </div>
+    );
+  }
 }
 
 export default App;
